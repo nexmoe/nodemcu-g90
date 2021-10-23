800509418b3d2e1024bd8283f6c97a6c6a5c90cb
Nexmoe <16796652+nexmoe@users.noreply.github.com>
2021-10-23 11:16:45 +0800

    Create main.ino

---
+++ main/main.ino
@@ -0,0 +1,98 @@
+#define BLINKER_WIFI
+#define BLINKER_MIOT_OUTLET //小米
+#include <Servo.h>
+ 
+#define PIN_SERVO D0 //舵机信号线
+ 
+Servo myservo;
+ 
+#include <Blinker.h>
+ 
+char auth[] = "";//点灯秘钥
+char ssid[] = "";//无线网名称
+char pswd[] = "";//无线网密码
+ 
+bool oState = false;
+
+//小米
+void miotPowerState(const String & state)
+{
+    BLINKER_LOG("need set power state: ", state);
+    myservo.attach(PIN_SERVO);
+ 
+    if (state == BLINKER_CMD_ON) {
+        myservo.write(29);//舵机正转度数
+        delay(1000); 
+        myservo.write(0);//舵机归位
+        
+        BlinkerMIOT.powerState("on");
+        BlinkerMIOT.print();
+ 
+        oState = true;
+    }
+    else if (state == BLINKER_CMD_OFF) {
+        myservo.write(-29);//舵机反转度数
+        delay(1000); 
+        myservo.write(0);//舵机归位
+        
+        BlinkerMIOT.powerState("off");
+        BlinkerMIOT.print();  
+ 
+        oState = false;
+    }
+}
+ 
+//小米
+void miotQuery(int32_t queryCode)
+{
+    BLINKER_LOG("MIOT Query codes: ", queryCode);
+ 
+    switch (queryCode)
+    {
+        case BLINKER_CMD_QUERY_ALL_NUMBER :
+            BLINKER_LOG("MIOT Query All");
+            BlinkerMIOT.powerState(oState ? "on" : "off");
+            BlinkerMIOT.print();
+            break;
+        case BLINKER_CMD_QUERY_POWERSTATE_NUMBER :
+            BLINKER_LOG("MIOT Query Power State");
+            BlinkerMIOT.powerState(oState ? "on" : "off");
+            BlinkerMIOT.print();
+            break;
+        default :
+            BlinkerMIOT.powerState(oState ? "on" : "off");
+            BlinkerMIOT.print();
+            break;
+    }
+}
+ 
+void dataRead(const String & data)
+{
+    BLINKER_LOG("Blinker readString: ", data);
+ 
+    Blinker.vibrate();
+    
+    uint32_t BlinkerTime = millis();
+    
+    Blinker.print("millis", BlinkerTime);
+}
+ 
+void setup()
+{
+    Serial.begin(115200);
+    BLINKER_DEBUG.stream(Serial);
+ 
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+ 
+    Blinker.begin(auth, ssid, pswd);
+    Blinker.attachData(dataRead);
+    
+    BlinkerMIOT.attachPowerState(miotPowerState);
+    BlinkerMIOT.attachQuery(miotQuery);
+}
+ 
+void loop()
+{
+    Blinker.run();
+}
